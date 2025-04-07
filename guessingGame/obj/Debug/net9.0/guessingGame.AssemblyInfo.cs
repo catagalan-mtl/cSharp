8479ec9a68a7bce9c81f5bf9ded7b48c24f33708
Catalina Galan <catalinagalanvargas@gmail.com>
2025-04-07 19:19:56 -0400

    adding last details of fixing deleted files mess

--- guessingGame/obj/Debug/net9.0/guessingGame.AssemblyInfo.cs
+++ guessingGame/obj/Debug/net9.0/guessingGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("guessingGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29c248e48dba344d2a36a7bbea7cebbd7fb1426f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+039f8188f9a6bd7d1270f8023f951e5d78672a5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("guessingGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("guessingGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

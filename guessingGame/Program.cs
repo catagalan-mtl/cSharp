29c248e48dba344d2a36a7bbea7cebbd7fb1426f
Catalina Galan <catalinagalanvargas@gmail.com>
2025-04-07 17:58:50 -0400

    working on cSharp learning projects

---
+++ guessingGame/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random random = new Random();
+int answer = random.Next(1, 100);
+
+Console.WriteLine("Hello, welcome to the Guessing Game! (shhhhhhhh): " + answer);
+Console.Write("Enter a number from 1 to 100: > ");
+byte choice;
+
+while ((!Byte.TryParse(Console.ReadLine(), out choice)) || (choice > 100))
+{
+  Console.Write("Wrong Input! Please tri again: > ");
+}
+
+while (choice != answer)
+{
+
+}
+Console.WriteLine("YOU WIN!!!!!!!");
